40910952977b2d5cdd7829174fac29a0b5ecbe11
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2019-04-06 17:20:40 +0530

    Modified the application.

--- Arcs19/app/src/main/java/com/example/arcs19/MainActivity.java
+++ Arcs19/app/src/main/java/com/example/arcs19/MainActivity.java
@@ -5,27 +5,53 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.LoginFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
 import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometerSensor, gyroscopeSensor, rotationSensor;
-    private float df_value0 = 0, df_value1 = 0, df_value2 = 0, value0, value1, value2, diff0, diff1, diff2;
+    private float value0, value1, value2;
     private boolean calibrate = false;
     private RelativeLayout currentLayout;
+
+    // bunch of location related apis
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+
+    private double latitude = 12.970151, longitude = 79.155673;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +67,44 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(MainActivity.this, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(MainActivity.this, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(MainActivity.this, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL);Random rand = new Random();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                // location is received
+                mCurrentLocation = locationResult.getLastLocation();
+                latitude = mCurrentLocation.getLatitude();
+                longitude = mCurrentLocation.getLongitude();
+            }
+        };
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("rash_drive").getRef();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Double value = Double.parseDouble(dataSnapshot.child("percent").getValue().toString());
+
+                if(value < 0.3) {
+                    currentLayout.setBackgroundResource(R.drawable.gradient_red);
+                }
+
+                else {
+                    currentLayout.setBackgroundResource(R.drawable.gradient_green);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -51,14 +114,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (calibrate) {
             FirebaseDatabase database = FirebaseDatabase.getInstance();
             DatabaseReference myRef = database.getReference("car_data");
-
+                                                                                                                                    Random number = new Random();
+                                                                                                                            double number_1 = number.nextDouble()/1000000;
             String timestamp = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-
             if (sensor_type.getType() == Sensor.TYPE_ACCELEROMETER) {
                 myRef.child(timestamp).child("accel_x").setValue(event.values[0]);
                 myRef.child(timestamp).child("accel_y").setValue(event.values[1]);
                 myRef.child(timestamp).child("accel_z").setValue(event.values[2]);
-//            Log.i("custom", "accel " + event.values[0] + " " + event.values[1] + " " + event.values[2]);
+                myRef.child(timestamp).child("latitude").setValue(latitude + number_1);
+                myRef.child(timestamp).child("longitude").setValue(longitude + number_1);
             }
 
             if (sensor_type.getType() == Sensor.TYPE_GYROSCOPE) {
@@ -66,29 +130,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 value1 = event.values[1];
                 value2 = event.values[2];
 
-                diff0 = value0 - df_value0;
-                diff1 = value1 - df_value1;
-                diff2 = value2 - df_value2;
-
-                if (diff2 < -0.3) {
-                    Log.i("custom", "right side");
-                    myRef.child(timestamp).child("steering").setValue("right_side");
-
-                } else if (diff2 > 0.3) {
-                    Log.i("custom", "left side");
-                    myRef.child(timestamp).child("steering").setValue("left_side");
-                } else {
-                    Log.i("custom", "neutral side");
-                    myRef.child(timestamp).child("steering").setValue("neutral_side");
-                }
-
-            }
-
-            if (sensor_type.getType() == Sensor.TYPE_ROTATION_VECTOR) {
                 myRef.child(timestamp).child("gyro_x").setValue(event.values[0]);
                 myRef.child(timestamp).child("gyro_y").setValue(event.values[1]);
                 myRef.child(timestamp).child("gyro_z").setValue(event.values[2]);
-//            Log.i("custom", "rotation " + event.values[0] + " " + event.values[1] + " " + event.values[2] + " " + event.values[3]);
+            }
+
+            if (sensor_type.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                myRef.child(timestamp).child("rot_x").setValue(event.values[0]);
+                myRef.child(timestamp).child("rot_y").setValue(event.values[1]);
+                myRef.child(timestamp).child("rot_z").setValue(event.values[2]);
             }
         }
     }
@@ -99,22 +149,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void calibrateSensor(View view) {
-        df_value0 = value0;
-        df_value1 = value1;
-        df_value2 = value2;
-
         calibrate = !calibrate;
 
         Button sensorControl = findViewById(R.id.sensorBtn);
 
         if(sensorControl.getText().equals("CALIBRATE")) {
             sensorControl.setText("STOP");
-            currentLayout.setBackgroundResource(R.color.colorBackground1);
         }
 
         else {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("rash_drive");
+            myRef.child("percent").setValue("1");
+
             sensorControl.setText("CALIBRATE");
-            currentLayout.setBackgroundResource(R.color.colorBackground);
         }
     }
 }
--- Arcs19/app/src/main/java/com/example/arcs19/MapActivity.java
+++ Arcs19/app/src/main/java/com/example/arcs19/MapActivity.java
@@ -1,13 +1,58 @@
 package com.example.arcs19;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.firebase.database.ThrowOnExtraProperties;
+
+public class MapActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Error with GPS.", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        if(mLocationManager != null) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("custom", "Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
     }
 }
